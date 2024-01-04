482e2d2b954c8450b62f10cf23d2e9c71702de4f
Swati Kumari Singh <swati.ksingh13@gmail.com>
2024-01-04 15:43:08 +0530

    code 2

---
+++ Sort_list.java
@@ -0,0 +1,72 @@
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head== null || head.next==null){
+            return head;
+        }
+        ListNode mid=findMid(head);
+        ListNode head2=mid.next;
+        mid.next=null;
+
+        head=sortList(head);
+        head2=sortList(head2);
+        ListNode newhead=merge(head, head2);
+        return newhead;
+    }
+    public ListNode findMid(ListNode h){
+        ListNode slow=h, fast=h;
+        while(fast.next != null && fast.next.next !=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode head=null, temp=null;
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        if(list1.val < list2.val){
+            head=list1;
+            list1=list1.next;
+        }
+        else{
+            head=list2;
+            list2=list2.next;
+        }
+
+        temp=head;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                temp.next=list1;
+                list1=list1.next;
+            }
+            else{
+                temp.next=list2;
+                list2=list2.next;
+            }
+            temp=temp.next;
+        }
+        if(list1 != null){
+            temp.next=list1;
+        }
+        else{
+            temp.next=list2;
+        }
+        return head;
+    }
+}
